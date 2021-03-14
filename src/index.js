4092b64ddbf05e9fe4cafe5dc12de346bfe25eff
AndrewPerepelkin <avp.tep@rambler.ru>
2021-03-14 16:55:05 +0300

    init: add task answer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let reverseN;
+    if (n > 0) {
+        reverseN = Number(String(n)
+            .split('')
+            .reverse()
+            .join('')
+        )
+    }
+
+    if (n < 0) {
+        reverseN = Number(String(-n)
+            .split('')
+            .reverse()
+            .join('')
+        )
+    }
+
+    return reverseN;
 }
